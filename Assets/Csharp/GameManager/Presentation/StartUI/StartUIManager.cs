1a9e05dda7df0672e220603133c33188b2a538ed
unknown <1186521289@qq.com>
2026-05-18 23:19:00 +0800

    补充

--- Assets/Csharp/GameManager/Presentation/StartUI/StartUIManager.cs
+++ Assets/Csharp/GameManager/Presentation/StartUI/StartUIManager.cs
@@ -6,11 +6,13 @@ namespace Csharp.GameManager.Presentation.StartUI
     public class StartUIManager : MonoBehaviour
     {
         public GameObject StartUI;
+        public GameObject CreatorUI;
 
         private void Awake()
         {
             if (StartUI == null) return;
             StartUI.SetActive(true);
+            CreatorUI.SetActive(false);
         }
 
         public void StartGame()
@@ -22,6 +24,16 @@ namespace Csharp.GameManager.Presentation.StartUI
         public void ExitGame()
         {
             Application.Quit();
+        }
+
+        public void OpenCreator()
+        {
+            CreatorUI.SetActive(true);
+        }
+        
+        public void CloseCreator()
+        {
+            CreatorUI.SetActive(false);
         }
     }
 }
