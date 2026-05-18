5b077ef88adf697333a4662cc91d0af379285c22
unknown <1186521289@qq.com>
2026-05-18 20:49:15 +0800

    开始界面脚本

---
+++ Assets/Csharp/GameManager/Presentation/StartUI/StartUIManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Csharp.GameManager.Presentation.StartUI
+{
+    public class StartUIManager : MonoBehaviour
+    {
+        public GameObject StartUI;
+
+        private void Awake()
+        {
+            if (StartUI == null) return;
+            StartUI.SetActive(true);
+        }
+
+        public void StartGame()
+        {
+            StartUI.SetActive(false);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
